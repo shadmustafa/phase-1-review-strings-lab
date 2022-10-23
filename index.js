a05d410fa6f68e6d80c249728dd50df8603d2b55
shad mustafa <shadmanmi2018@gmail.com>
2022-10-23 15:31:45 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+let currentUser = 'shad';
+
+const welcomeMessage = "Welcome to Flatbook, "+currentUser+" !";
+
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+upperCaseCurrentUser+" !";
+
+// const shortGreeting="Welcome, "+currentUser.charAt(0)+" !";
+const firstInitial = currentUser[0];
+// const restOfName = currentUser.slice(1);
+const shortGreeting="Welcome, "+firstInitial+" !";
